a85fa6033cc2da625aa4f03e1ad06c969f597fbf
bardzim <bardzim@gmail.com>
2023-02-18 18:25:49 +0400

    add keys in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', init)
+
+const opts = ['*', '/', '+', '-', '9','8','7','6','5','4','3','2','1','0','.']; //all keys 
+const spec = ['*', '/', '+', '-'] // special funtion keys
+
+function init() {
+    document.title = "JavaScript Project"
+}
