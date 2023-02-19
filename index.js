bec4ef183728096a5c6553306ab3c2b5ba050bc5
bardzim <bardzim@gmail.com>
2023-02-20 02:48:27 +0400

    creating fields with dom

--- index.js
+++ index.js
@@ -5,4 +5,28 @@ const spec = ['*', '/', '+', '-'] // special funtion keys
 
 function init() {
     document.title = "JavaScript Calculator"
+
+    //container for calculator
+    const container = document.createElement('div');
+    container.classList.add('container');
+    container.style.maxWidth = '600px';
+    container.style.margin = 'auto';
+    document.body.appendChild(container);
+
+    //output field
+    const output = document.createElement('input');
+    output.setAttribute('type', 'text');
+    output.classList.add('output');
+    output.style.width = '100%';
+    output.style.lineHeight = '50px';
+    output.style.fontSize = '3em';
+    output.style.textAlign = 'right';
+    container.appendChild(output);
+
+    //main container for keys
+    const main = document.createElement('div');
+    main.classList.add('main');
+    main.style.width = '100%';
+    container.appendChild(main)
+
 }
